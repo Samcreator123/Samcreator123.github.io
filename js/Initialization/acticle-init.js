21a93be4fe6b38ff48cf254e41f04c9d51d0bd93
22004941NICK\2200531 <2200531@systex.com.tw>
2024-07-10 18:11:29 +0800

    大綱差浮動

--- js/Initialization/acticle-init.js
+++ js/Initialization/acticle-init.js
@@ -1,33 +1,37 @@
 import Theme from '/js/theme.js';
-import {remove_dot_html} from '/js/remove-dot-html.js';
-import {loadTemplate} from '/js/template_loader.js';
+import { remove_dot_html } from '/js/remove-dot-html.js';
+import { loadTemplate } from '/js/template_loader.js';
 import CodeSnippet from '/js/code_snippet.js'
 import config from '/js/config.js';
 import Navbar from '/js/adjustable_navbar.js'
+import Outline from '/js/outline.js'
 
 // 用 load 會跑不出來
-document.addEventListener('DOMContentLoaded',()=>{
-    
+document.addEventListener('DOMContentLoaded', () => {
+
     const theme = new Theme('theme-toggle')
     // 載入 header 的html、亮暗主題、navbar 的裝置切換
     loadTemplate('/html-template/header.html', 'header-template')
-    .then(() =>{
-        return theme.setDefault();
-    })
-    .then(()=>{
-        return theme.setAddEventListenerIntoButton();
-    })
-    .then(()=>{
-        const navbar = new Navbar('navbar','menu-toggle')
-        navbar.toggle_navbar();
-    })
-    .catch(error => console.error('Error loading template:',error))
-    
+        .then(() => {
+            return theme.setDefault();
+        })
+        .then(() => {
+            return theme.setAddEventListenerIntoButton();
+        })
+        .then(() => {
+            const navbar = new Navbar('navbar', 'menu-toggle')
+            navbar.toggle_navbar();
+        })
+        .catch(error => console.error('Error loading template:', error))
+
     loadTemplate('/html-template/footer.html', 'footer-template');
-    
+
     remove_dot_html(config.isDevelopmentEnv);
-    
+
     const code_snippet = new CodeSnippet();
-    code_snippet.select_this_code('.code-container','code');
-    code_snippet.copy_closest_code_when_press_button('.copy_button','.code-container','code');
+    code_snippet.select_this_code('.code-container', 'code');
+    code_snippet.copy_closest_code_when_press_button('.copy_button', '.code-container', 'code');
+
+    const outline = new Outline('^outline.*$', '^anchor.*$');
+    outline.setGoToLogic();
 });
--- js/outline.js
+++ js/outline.js
@@ -1,40 +1,76 @@
 class Outline {
     #patternOfOutline = '';
     #patternOfAnchor = '';
-    #allElement = [];
+    #allElement = new Set();
 
     constructor(patternOfOutline, patternOfAnchor) {
         this.#patternOfOutline = patternOfOutline;
         this.#patternOfAnchor = patternOfAnchor;
-        this.allElements = document.querySelectorAll('*');
+        document.querySelectorAll('*').forEach(o => {
+            this.#allElement.add(o.id)
+        })
     }
 
     setGoToLogic() {
-        // 找到符合 patternOfOutline 的超連結元素
-        var outlines = this.#findElementMatchPattern('^outline.*$')
-        // 找到符合 patternOfAnchor 的超連結元素
-        var outlines = this.#findElementMatchPattern('^anchor.*$')
-        outlines.forEach(outline => {
-            document.getElementById(outline).addEventListener('click', () => {
+
+        // 找到符合 patternOfAnchor 的超連結元素 '^anchor.*$'
+        let anchorDic = this.#turnIntoDictionary('.', 1, this.#findElementMatchPattern(this.#patternOfAnchor));
+
+        // 找到符合 patternOfOutline 的超連結元素 '^outline.*$'
+        let outlinesDic = this.#turnIntoDictionary('.', 1, this.#findElementMatchPattern(this.#patternOfOutline));
+
+        for (let key in outlinesDic) {
+            let outlineId = outlinesDic[key];
+            document.getElementById(outlineId).addEventListener('click', () => {
+                let anchorId = anchorDic[key];
+
+                if (!anchorId) return;
                 // 長一樣的拼裝再一起
+                let anchorElement = document.getElementById(anchorId);
+
                 // 調整至視窗範圍 outline.click => auchor.scrollIntoView() 
+                anchorElement.scrollIntoView(this.#setMovementLogic());
+
                 // 高亮離錨點最近的section
-
+                this.#setNearestSectionHightLight(anchorElement, 2000);
             })
-
-        });
+        }
     }
 
     #findElementMatchPattern(patterns) {
         const regex = new RegExp(patterns);
-        return Array.from(this.#allElement).filter(element => regex.test(element.id));
+        return Array.from(this.#allElement).filter(element => regex.test(element));
+    }
+
+    #turnIntoDictionary(symbolOfSplit, key_index, elements) {
+        let dic = {};
+
+        elements.forEach(element => {
+            let key = element.split(symbolOfSplit)[key_index];
+            dic[key] = element;
+        })
+
+        return dic;
     }
 
     #setMovementLogic() {
-
+        return {
+            behavior: "smooth",
+            block: "start",
+            inline: "center",
+        }
     }
 
-    #setHightLight() {
+    #setNearestSectionHightLight(anchorElement, milisecond) {
+        const closestSection = anchorElement.closest('section');
+        if (closestSection) {
+            closestSection.classList.add('high-light');
 
+            setTimeout(() => {
+                closestSection.classList.remove('high-light');
+            }, milisecond)
+        }
     }
 }
+
+export default Outline
