3b467a2e4809caa89fe2f6f2ce102356d9dd2155
22004941NICK\2200531 <2200531@systex.com.tw>
2024-07-15 17:59:09 +0800

    大綱放旁邊，準備潤稿、準備搜得道

--- js/template_loader.js
+++ js/template_loader.js
@@ -1,16 +1,14 @@
-export function loadTemplate(url, elementId)
-{
-    return new Promise((resolve, reject)=>{
-
+export function loadTemplate(url, elementId) {
+    return new Promise((resolve, reject) => {
         fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(elementId).insertAdjacentHTML('beforeend',data);
-            resolve();
-        })
-        .catch(error=>{
-            console.error('Error loading template:', error);
-            reject(error);
-        })
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById(elementId).insertAdjacentHTML('beforeend', data);
+                resolve();
+            })
+            .catch(error => {
+                console.error('Error loading template:', error);
+                reject(error);
+            })
     })
 }
